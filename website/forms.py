ab1ea79f97524f76e49d7b283bcf251be5e97af2
nath <nath12009300806>
2017-11-21 14:55:50 -0600

    Added create new monograph

--- website/forms.py
+++ website/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.forms import ModelForm
+from django.utils.translation import ugettext_lazy as _
+
 from website.models import Monograph
 
 
@@ -7,3 +10,16 @@ class MonographForm(forms.Form):
     class Meta:
         model = Monograph
         fields = '__all__'
+
+
+class NewMonographForm(ModelForm):
+
+    class Meta:
+        model = Monograph
+        fields = ['number', 'primary_name', 'secondary_name']
+
+        labels = {
+            'number': _('Número'),
+            'primary_name': _('Nombre 1'),
+            'secondary_name': _('Nombre 2')
+        }
--- website/views.py
+++ website/views.py
@@ -1,6 +1,9 @@
-from django.views.generic import ListView, UpdateView
+from django.http import Http404
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView, CreateView
 
-from website.forms import MonographForm
+from website.forms import MonographForm, NewMonographForm
 from website.models import Monograph
 
 
@@ -14,14 +17,24 @@ class MainView(ListView):
         return context
 
 
-class NewMonographView(ListView):
-    template_name = 'monograph_list_template.html'
+class NewMonographView(CreateView):
+    template_name = 'create_monograph.html'
     model = Monograph
+    form_class = NewMonographForm
 
     def get_context_data(self, **kwargs):
         context = super(NewMonographView, self).get_context_data(**kwargs)
-        context['monographs'] = Monograph.objects.all()
         return context
+
+    def form_valid(self, form):
+        number = form.data['number']
+        primary_name = form.data['primary_name']
+        secondary_name = form.data['secondary_name']
+        try:
+            monograph = Monograph.objects.create(number=number, primary_name=primary_name, secondary_name=secondary_name)
+        except:
+            raise Http404
+        return reverse_lazy('main_page')
 
 
 class UpdateMonographView(UpdateView):
