b9037b40577c5e6c83c85c9a066dffb1b2088db9
ademir501 <ademirmorales501@gmail.com>
2017-12-04 18:18:37 -0600

    Update settings.py

--- Monografias/settings.py
+++ Monografias/settings.py
@@ -124,5 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
