ead252030f40222ba3f0bfe6daa13d97a58f0294
ademir501 <ademirmorales501@gmail.com>
2017-12-04 18:45:51 -0600

    Update settings.py

--- Monografias/settings.py
+++ Monografias/settings.py
@@ -147,6 +147,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
