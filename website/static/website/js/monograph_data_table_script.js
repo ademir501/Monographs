e247f37685fea7e7baccdb1cbe68f0914e85dc4c
EDGAR ADEMIR MORALES PEREZ <emoralesp1302@egresado.ipn.mx>
2019-05-10 17:23:48 +0900

    testing

--- website/static/website/js/monograph_data_table_script.js
+++ website/static/website/js/monograph_data_table_script.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
         }
     });
 
-     $('#myModal').on('shown.bs.modal', function () {
+     $('#exampleModal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
 })
 
